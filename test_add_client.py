0f2f76d27ff2ec6fb9cfd6af1951a8b6d16b4c7f
Andrey Vassel <kapral.dtr16@gmail.com>
2019-11-27 20:49:27 +0700

    script for adding client

--- test_add_client.py
+++ test_add_client.py
@@ -7,7 +7,7 @@ from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
 import unittest, time, re
 
-class TestAddGroup(unittest.TestCase):
+class Client(unittest.TestCase):
     def setUp(self):
         self.driver = webdriver.Firefox()
         self.driver.implicitly_wait(30)
@@ -15,44 +15,14 @@ class TestAddGroup(unittest.TestCase):
         self.verificationErrors = []
         self.accept_next_alert = True
     
-    def test_add_group(self):
+    def test_client(self):
         driver = self.driver
-        driver.get("http://localhost/addressbook/group.php")
+        driver.get("http://localhost/addressbook/")
         driver.find_element_by_name("user").click()
-        driver.find_element_by_name("user").click()
-        driver.find_element_by_name("user").click()
-        driver.find_element_by_name("user").click()
-        driver.find_element_by_name("user").clear()
-        driver.find_element_by_name("user").send_keys("admin")
-        driver.find_element_by_id("LoginForm").click()
-        driver.find_element_by_name("pass").click()
         driver.find_element_by_name("pass").clear()
         driver.find_element_by_name("pass").send_keys("secret")
-        driver.find_element_by_xpath("//input[@value='Login']").click()
-        driver.find_element_by_link_text("groups").click()
-        driver.find_element_by_name("new").click()
-        driver.find_element_by_name("group_name").click()
-        driver.find_element_by_name("group_name").clear()
-        driver.find_element_by_name("group_name").send_keys("test1")
-        driver.find_element_by_name("group_header").click()
-        driver.find_element_by_name("group_header").clear()
-        driver.find_element_by_name("group_header").send_keys("test1")
-        driver.find_element_by_name("group_footer").click()
-        driver.find_element_by_name("group_footer").clear()
-        driver.find_element_by_name("group_footer").send_keys("test1")
-        driver.find_element_by_name("submit").click()
-        driver.find_element_by_link_text("group page").click()
-        driver.find_element_by_link_text("Logout").click()
-        driver.find_element_by_name("user").click()
-        driver.find_element_by_name("user").click()
-        driver.find_element_by_name("user").click()
-        driver.find_element_by_name("user").click()
         driver.find_element_by_name("user").clear()
         driver.find_element_by_name("user").send_keys("admin")
-        driver.find_element_by_id("LoginForm").click()
-        driver.find_element_by_name("pass").click()
-        driver.find_element_by_name("pass").clear()
-        driver.find_element_by_name("pass").send_keys("secret")
         driver.find_element_by_xpath("//input[@value='Login']").click()
         driver.find_element_by_link_text("add new").click()
         driver.find_element_by_name("firstname").click()
@@ -63,24 +33,25 @@ class TestAddGroup(unittest.TestCase):
         driver.find_element_by_name("lastname").send_keys("Ekaterina")
         driver.find_element_by_name("address").click()
         driver.find_element_by_name("address").clear()
-        driver.find_element_by_name("address").send_keys("Shumakova 23a")
+        driver.find_element_by_name("address").send_keys("Shumakova, 23a")
         driver.find_element_by_name("home").click()
         driver.find_element_by_name("home").clear()
         driver.find_element_by_name("home").send_keys("123456")
         driver.find_element_by_name("mobile").click()
         driver.find_element_by_name("mobile").clear()
-        driver.find_element_by_name("mobile").send_keys("987456321")
+        driver.find_element_by_name("mobile").send_keys("987654321")
         driver.find_element_by_name("work").click()
         driver.find_element_by_name("work").clear()
-        driver.find_element_by_name("work").send_keys("635241")
+        driver.find_element_by_name("work").send_keys("456123")
         driver.find_element_by_name("email").click()
         driver.find_element_by_name("email").clear()
-        driver.find_element_by_name("email").send_keys("tester@mail.ru")
+        driver.find_element_by_name("email").send_keys("user1@mail.ru")
         driver.find_element_by_name("email2").click()
         driver.find_element_by_name("email2").clear()
-        driver.find_element_by_name("email2").send_keys("tester11@mail.ru")
+        driver.find_element_by_name("email2").send_keys("user2@mail.ru")
         driver.find_element_by_xpath("(//input[@name='submit'])[2]").click()
         driver.find_element_by_link_text("home").click()
+        driver.find_element_by_link_text("Logout").click()
     
     def is_element_present(self, how, what):
         try: self.driver.find_element(by=how, value=what)
