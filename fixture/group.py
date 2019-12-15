bcc1a7e3272c6c07def4a62830b3716191f73a20
Andrey Vassel <kapral.dtr16@gmail.com>
2019-12-15 22:06:32 +0700

    checks in the form of comparison of lists for groups

--- fixture/group.py
+++ fixture/group.py
@@ -1,3 +1,6 @@
+from model.group import Group
+
+
 class GroupHelper:
 
     def __init__(self, app):
@@ -6,10 +9,8 @@ class GroupHelper:
     def create(self, group):
         wd = self.app.wd
         self.open_group_page()
-        # new group
         wd.find_element_by_name("new").click()
         self.fill_group_form(group)
-        # submit new group
         wd.find_element_by_name("submit").click()
         self.open_group_page()
 
@@ -35,7 +36,6 @@ class GroupHelper:
         wd = self.app.wd
         self.open_group_page()
         self.select_first_group()
-        # submit deletion
         wd.find_element_by_name("delete").click()
         self.open_group_page()
 
@@ -59,3 +59,13 @@ class GroupHelper:
         wd = self.app.wd
         self.open_group_page()
         return len (wd.find_elements_by_name("selected[]"))
+
+    def get_group_list(self):
+        wd = self.app.wd
+        self.open_group_page()
+        groups=[]
+        for element in wd.find_elements_by_css_selector("span.group"):
+            text=element.text
+            id=element.find_element_by_name("selected[]").get_attribute("value")
+            groups.append(Group(name=text, id=id))
+        return groups
