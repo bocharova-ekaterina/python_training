4f5abf5b0f4c0dc0e307de62d512176bdea09312
Andrey Vassel <kapral.dtr16@gmail.com>
2020-01-05 17:35:18 +0700

    adding test parameterization (task №15)

--- tests/test_add_client.py
+++ tests/test_add_client.py
@@ -1,11 +1,31 @@
 # -*- coding: utf-8 -*-
 from model.client import Client
+import string
+import random
+import pytest
 
 
-def test_client(app):
+def random_string(prefix, maxlen):
+    symbols = string.ascii_letters + string.digits + " " * 10 + string.punctuation
+    return prefix + " ".join([random.choice(symbols) for i in range (random.randrange(maxlen))])
+
+
+testdata=[
+        Client(firstname=firstname, lastname=lastname, address=address, home=home, mobile=mobile, work=work, email=email, email2=email2)
+        for firstname in [random_string("firstname", 10)]
+        for lastname in [" ", random_string("lastname", 10)]
+        for address in [" ", random_string("address", 20)]
+        for home in [" ", random_string("home", 10)]
+        for mobile in [" ", random_string("mobile", 10)]
+        for work in [" ", random_string("work", 10)]
+        for email in [" ", random_string("email", 20)]
+        for email2 in [" ", random_string("email2", 20)]
+]
+
+@pytest.mark.parametrize("client", testdata, ids=[repr(x) for x in testdata])
+
+def test_client(app, client):
     old_clients = app.client.get_client_list()
-    client = Client(firstname="Ekaterina", lastname="Bocharova", address="Shumakova, 23a",
-                                      homephone="123456", mobilephone="987654321", workphone="456123", email="user1@mail.ru", email2="user2@mail.ru")
     app.client.add_client(client)
     assert len(old_clients) + 1 == app.client.count()
     new_clients = app.client.get_client_list()
